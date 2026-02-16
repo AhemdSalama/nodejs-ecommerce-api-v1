68092df75a077e97029f313cb8efe3815b98401a
Ahmed Salama <ahmedsalamafcis@gmail.com>
2026-02-17 01:48:54 +0200

    updating files

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({path:"config.env"}); // dotenv.config path the values in the file to the global process variable
+const morgan = require('morgan');
 
 const app = express();
 
+if(process.env.NODE_ENV === "development")
+{
+    app.use(morgan("dev"));
+    console.log(`Mode: ${process.env.NODE_ENV}`);
+}
 
 app.get('/',(req,res)=>{
-    res.send("Our API V3");
+    res.send("Our API V4");
 })
 
 
-
+const PORT = process.env.PORT || 8000;
 // Listen on the port 
-app.listen(8000,()=>{
-    console.log("App running");
+app.listen(PORT,()=>{
+    console.log(`App is running on port ${PORT}`);
 })
